e7a98d5710588608f560f504f802638638554daf
Mohana Rao <mohana.r@samsung.com>
2018-12-06 14:56:36 -0800

    Merging OpticFlow related changes from flownet_test branch.

--- pytorch/exp/flownet_test.py
+++ pytorch/exp/flownet_test.py
@@ -8,17 +8,18 @@ args = [
     '--name', __file__.split('/')[-1].split('.')[0],  # name is filename
     '--print-freq', '1',
     '--dataset', 'charadesflow',
-    '--data','/scratch/gsigurds/Charades_v1_flow/',
     '--arch', 'vgg16flow',
+    '--data','../datasets/Charades_v1_flow/',
     '--lr', '5e-3',
     '--lr-decay-rate','15',
     '--epochs','40',
-    '--batch-size', '64',
+    '--batch-size', '8',
     '--train-size', '0.2',
     '--val-size', '0.001',
-    '--cache-dir', '/nfs.yoda/gsigurds/ai2/caches/',
+    '--cache-dir', 'temp/gsigurds/ai2/caches/',
     '--pretrained',
-    '--resume', './twostream_flow.pth',
+    '--pretrained-weights', '../datasets/vgg16flow_ucf101.pth',
+#     '--resume', './twostream_flow.pth',
     '--evaluate',
 ]
 sys.argv.extend(args)
